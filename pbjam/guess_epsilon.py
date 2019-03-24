666bf4660db71cd09f6c5154b131dea976f229e5
grd349 <grd349@gmail.com>
2019-03-24 10:24:28 +0000

    Added simple Vrard implementation that m,ay or may not work for giants.

---
+++ pbjam/guess_epsilon.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+class epsilon():
+    ''' A class to predict epsilon TODO
+
+    Attributes
+    ----------
+    method : string
+        Sets the method used to estimat eepsilon
+    vrard_dict : dict
+        Stores the Vrard coefficients
+    '''
+    def __init__(self, method='Vrard'):
+        self.method = method
+        # 0.601 + 0.632 logh∆νi
+        self.vrard_dict = {'alpha': 0.601, 'beta': 0.632}
+
+    def vrard(self, dnu):
+        ''' Calculates epsilon prediction from Vrard 2015
+
+        Uses the equation from Table 1.
+
+        Has no dependence on temperature so not reliable.
+
+        Assumes a fixed uncertainty of 0.1.
+        '''
+        return [self.vrard_dict['alpha'] +
+                self.vrard_dict['alpha'] * np.log10(dnu), 0.1]
+
+    def __call__(self, dnu, numax=-1, teff=-1, dnu_err=-1, numax_err=-1, teff_err=-1):
+        ''' Calls the relevant defined method and returns an estimate of
+        epsilon.
+
+        Inputs
+        ------
+        dnu : real
+            Large frequency spacing
+        numax : real
+            Frequency of maximum power
+        teff : real
+            Stellar effective temperature
+        dnu_err : real
+            Uncertainty on dnu
+        numax_err : real
+            uncertainty on numax
+        teff_err : real
+            uncertainty on teff
+
+        Returns
+        -------
+        result : array-like
+            [estimate of epsilon, unceritainty on estimate]
+        '''
+        if self.method == 'Vrard':
+            if numax > 288.0:
+                print('Vrard method really only valid for Giants')
+                return [self.vrard(dnu)[0], 1.0]
+            return self.vrard(dnu)
---
+++ pbjam/tests/test_epsilon_guess.py
@@ -0,0 +1,22 @@
+from __future__ import division, print_function
+
+import pytest
+import numpy as np
+from numpy.testing import (assert_almost_equal, assert_array_equal,
+                           assert_allclose)
+
+from pbjam import guess_epsilon
+
+def test_epsilon_vrard_giant():
+    eps = guess_epsilon.epsilon(method='Vrard')
+    # Test a simple red giant result
+    result = eps(4.5)
+    assert_almost_equal(result[0], 1.0, 0.1)
+    assert_almost_equal(result[1], 0.1, 0.01)
+
+
+def test_epsilon_vrard_dwarf():
+    eps = guess_epsilon.epsilon(method='Vrard')
+    # Check unc high for the Sun
+    result = eps(135.0, 3050.0)
+    assert_almost_equal(result[1], 1.0, 0.01)
