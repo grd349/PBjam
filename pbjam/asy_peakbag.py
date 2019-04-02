801dc13553de345139228badfdd8856d8e1e9b21
nielsenmb <martinbonielsen.astro@gmail.com>
2019-04-02 17:09:02 +0100

    comments, docstrings, readability

--- pbjam/asy_peakbag.py
+++ pbjam/asy_peakbag.py
@@ -15,7 +15,7 @@ class model():
         self.f = f
 
     def lor(self, freq, h, w):
-        ''' Lorentzian to describe a mode.
+        """ Lorentzian to describe a mode.
         
         Parameters
         ----------
@@ -30,14 +30,14 @@ class model():
         ------- 
         mode : array
             The mode power distribution in frequency bins
-        '''
+        """
      
         w = 10**(w)
         
         return h / (1.0 + 4.0/w**2*(self.f - freq)**2)
 
     def pair(self, freq0, h, w, d02, hfac=0.7):
-        ''' Define a pair as the sum of two Lorentzians
+        """ Define a pair as the sum of two Lorentzians
         
         A pair is assumed to consist of an l=0 and an l=2 mode.
         The widths are assumed to be identical, and the height
@@ -57,19 +57,20 @@ class model():
             The small separation (muHz)
         hfac : float, optional
             Ratio of the l=2 height to that of l=0 (unitless)
+            
         Returns
         -------
-        pair : array
+        pair_model : array
             The power distribution in frequency of a mode pair 
             (?, spectrum units)
-        '''
+        """
         
-        model = self.lor(freq0, h, w)
-        model += self.lor(freq0 - d02, h*hfac, w)
-        return model
+        pair_model = self.lor(freq0, h, w)
+        pair_model += self.lor(freq0 - d02, h*hfac, w)
+        return pair_model
 
     def asy(self, numax, dnu, d02, eps, alpha, hmax, Envwidth, w):
-        '''Constructs a spectrum model from the asymptotic relation
+        """Constructs a spectrum model from the asymptotic relation
         
         The asymptotic relation for p-modes is defined as:
         nu_nl = (n + epsilon + alpha/2(n - nmax)**2) * log_dnu
@@ -84,7 +85,7 @@ class model():
         dnu : float
             Large separation (muHz)
         d02 : float
-            Small separation (muHz)
+            Small separation (units of dnu)
         eps : float
             Phase term of the asymptotic relation (unitless)
         alpha : float
@@ -100,10 +101,10 @@ class model():
         -------
         model : array
             spectrum model around the p-mode envelope
-        '''
+        """
         
         nmax = numax / dnu - eps
-        nn = np.arange(np.floor(nmax-5), np.floor(nmax+6), 1) # Why is floor used here? Hard code?
+        nn = np.arange(np.floor(nmax-5), np.floor(nmax+6), 1) # (hard code values)
         model = np.ones(len(self.f)) 
         for n in nn:
             f0 = (n + eps + alpha/2*(n - nmax)**2) * dnu # asymptotic relation
@@ -112,7 +113,7 @@ class model():
         return model
 
     def __call__(self, p):
-        '''Calls model of the asymptotic relation 
+        """Calls model of the asymptotic relation 
         
         Parameters
         ----------
@@ -123,28 +124,66 @@ class model():
         -------
         model : array
             spectrum model around the p-mode envelope
-        '''
+        """
         return self.asy(*p)
 
 class Prior(epsilon):
-    def __init__(self, _bounds, _gaussian):
-        self.bounds = _bounds
-        self.gaussian = _gaussian
-        self.data_file = PACKAGEDIR + os.sep + 'data' + os.sep + 'rg_results.csv'
-        self.seff_offset = 4000.0 #What is this?
-        self.read_prior_data()
-        self.make_kde()
+    def __init__(self, bounds, gaussian):
+        """ 
+        Parameters
+        ----------
+        bounds : list
+            list of upper and lower bounds for the priors on the fit parameters
+        gaussian : ?
+            ???
+        """
+        
+        self.bounds = bounds
+        self.gaussian = gaussian
+        
+        self.data_file = os.path.join(*[PACKAGEDIR,'data','rg_results.csv']) # is this better/worse, slower/faster?
+        self.seff_offset = 4000.0 #Norm. const. for same KDE bandwidth on all axes (hard code values)
+        self.read_prior_data() #Inherited from epsilon
+        self.make_kde() #Inherited from epsilon, assigns the kde attribute
 
     def pbound(self, p):
-        ''' prior boundaries '''
+        ''' Check if proposed step is out of bounds
+       
+        Effectively truncates any prior beyond the supplied bounds.
+        
+        Parameters
+        ----------
+        p : array
+            Array containing mcmc proposal
+        
+        Returns
+        -------
+        prior : float or inf
+            If within bounds returns 0, -inf if not
+        '''
+        
         for idx, i in enumerate(p):
-            if ((self.bounds[idx][0] != 0) & (self.bounds[idx][1] != 0)):
+            if all(self.bounds[idx] != 0): #((self.bounds[idx][0] != 0) & (self.bounds[idx][1] != 0)):
                 if ((i < self.bounds[idx][0]) | (i > self.bounds[idx][1])):
                     return -np.inf
         return 0
 
     def pgaussian(self, p):
-        ''' Guassian priors - not yet implemented!!!'''
+        """ Guassian priors - not yet implemented!!!
+        
+        ????
+        
+        Parameters
+        ----------
+        p : array
+            Array containing mcmc proposal
+            
+        Return
+        ------
+        lnprior : float
+            ???
+        """
+        
         lnprior = 0.0
         for idx, i in enumerate(p):
             if self.gaussian[idx][1] != 0:
@@ -153,13 +192,31 @@ class Prior(epsilon):
         return lnprior
 
     def __call__(self, p):
+        """ Evaluate the prior on a proposed set of parameters
+        
+        The prior is estimated by a KDE of a large set of previous Kepler 
+        observations.
+        
+        Parameters
+        ----------
+        p : array
+            Array containing mcmc proposal
+            
+        Return
+        ------
+        lp : float
+            The prior evaluated at the proposed mcmc step
+        """
+        
         if self.pbound(p) == -np.inf:
             return -np.inf
+        
+        # Evaluate the prior, defined by a KDE
         lp = self.kde([np.log10(p[1]), np.log10(p[0]), p[8], p[3]])
         return lp
 
 class mcmc():
-    def __init__(self, f, snr, x0):
+    def __init__(self, f, snr, x0):      
         self.f = f
         self.snr = snr
         self.sel = np.where(np.abs(self.f - x0[0]) < 3*x0[1])
@@ -167,9 +224,15 @@ class mcmc():
         # numax, dnu, d02, eps, alpha, hmax, Envwidth, w, seff
         # TODO - tidy this bit up!
         self.seff_offset = 4000.0
-        bounds = [[x0[0]*0.9, x0[0]*1.1], [x0[1]*0.9, x0[1]*1.1],
-                   [0.04, 0.2], [0.5, 2.5], [0, 1], [x0[5]*0.5, x0[5]*1.5],
-                   [x0[6]*0.9, x0[6]*1.1], [-2, 1.0], [2.0, 4.0]]
+        bounds = [[x0[0]*0.9, x0[0]*1.1], 
+                  [x0[1]*0.9, x0[1]*1.1],
+                  [0.04, 0.2], 
+                  [0.5, 2.5], 
+                  [0, 1], 
+                  [x0[5]*0.5, x0[5]*1.5],
+                  [x0[6]*0.9, x0[6]*1.1], 
+                  [-2, 1.0], 
+                  [2.0, 4.0]]
         self.lp = Prior(bounds, [(0,0) for n in range(len(x0))])
 
     def likelihood(self, p):
@@ -186,7 +249,7 @@ class mcmc():
         ndim = len(x0)
         p0 = [np.array(x0) + np.random.rand(ndim)*1e-3 for i in range(nwalkers)]
         sampler = emcee.EnsembleSampler(nwalkers, ndim, self.likelihood)
-        print('Burmingham')
+        print('Burning ham')
         sampler.run_mcmc(p0, niter)
         sampler.reset()
         print('Sampling')
