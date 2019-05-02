10a7b53acda8acc8740aa305c0c7c80ea6ca30f2
nielsenmb <martinbonielsen.astro@gmail.com>
2019-05-02 11:33:24 +0100

    minor adjustments

--- pbjam/jar.py
+++ pbjam/jar.py
@@ -1,28 +1,27 @@
 """ Setup jam sessions and perform mode ID and peakbagging
 
 This jar contains the input layer for setting up jam sessions for peakbagging
-solar-like oscillators. 
+solar-like oscillators.
 
 A jam session is started by initializing the session class instance with a
-target ID, numax, large separation, and effective temperature, or lists of 
-these if you are working on multiple targets. Alternatively a dataframe or 
+target ID, numax, large separation, and effective temperature, or lists of
+these if you are working on multiple targets. Alternatively a dataframe or
 dictionary can also be provided, with columns corresponding to the above
-keywords. 
+keywords.
 
 Specific quarters, campgains or sectors can be requested in a kwargs dictionary
-with the relevant keyword (i.e., 'quarter' for KIC, etc.) 
+with the relevant keyword (i.e., 'quarter' for KIC, etc.)
 
 Once initialized, the session class contains a list of star class instances
-for each requested target, with corresponding SNR spectra for each. 
+for each requested target, with corresponding SNR spectra for each.
 
-The next step is to perform a mode ID on the SNR spectra. At the moment PBjam 
-only supports use of the asymptotic relation mode ID method. 
+The next step is to perform a mode ID on the SNR spectra. At the moment PBjam
+only supports use of the asymptotic relation mode ID method.
 
 Note
 ----
 Target IDs must be resolvable by Lightkurve
 """
-
 
 import lightkurve as lk
 from pbjam.asy_peakbag import asymptotic_fit
@@ -31,44 +30,44 @@ import astropy.units as units
 
 
 def bouncer(X):
-    """ Turn elements of X into lists, and check their length
-    
+    """ Turn elements of X into lists, and check their lengths are the same
+
     Parameters
     ----------
     X : list
         List of objects to be turned into list of lists of objects (yeah...)
-    
+
     Returns
     -------
     X : list
         List of lists of objects that was formerly just a list of objects
     """
-    
-    # TODO - this should probably be split into to, one to enforce list type 
+
+    # TODO - this should probably be split into to, one to enforce list type
     # and one to enforce all items in list of lists must have same length
     # right now ID, numax, dnu, teff are separate from kwarg arguments
     lens = []
     # Check that all elements of X are lists, and if not, make them so
-    for i,x in enumerate(X):        
+    for i, x in enumerate(X):
         if not isinstance(x, (list, np.ndarray, tuple)):
-            X[i] = [x]                  
+            X[i] = [x]
         lens.append(len(X[i]))
     # Check that all elements of X are the same length
     assert lens[1:] == lens[:-1], "Provided inputs must be same length"
-    
     return X
-   
+
+
 def get_psd_from_lk(ID, lkargs):
     """ Use Lightkurve to get snr
-    
-    Querries MAST using Lightkurve, based on the provided target ID(s) and 
-    observing season number. Then computes the periodogram based on the 
-    downloaded time series. 
-    
+
+    Querries MAST using Lightkurve, based on the provided target ID(s) and
+    observing season number. Then computes the periodogram based on the
+    downloaded time series.
+
     Parameters
     ----------
     ID : str, list of strs
-        String or list of strings of target IDs that Lightkurve can resolve. 
+        String or list of strings of target IDs that Lightkurve can resolve.
         (KIC, TIC, EPIC).
     lkargs : dict
         Dictionary of keywords for Lightkurve to get the correct observing
@@ -77,57 +76,53 @@ def get_psd_from_lk(ID, lkargs):
         month : for Kepler targets, applies to short-cadence data
         sector : for TESS targets
         campaign : for K2 targets
-        
+
     Returns
     -------
     PS_list : list of tuples
-        List of tuples for each requested target. First column of a tuple is 
-        frequency, second column is power. 
+        List of tuples for each requested target. First column of a tuple is
+        frequency, second column is power.
     """
+
     PS_list = []
-    
     for i, id in enumerate(ID):
-
-        lc = lk.search_lightcurvefile(target=id, 
-                                      quarter=lkargs['quarter'][i], 
-                                      campaign=lkargs['campaign'][i],
-                                      sector=lkargs['sector'][i], 
-                                      month = lkargs['month'][i]).download().PDCSAP_FLUX
-         
+        tgt = lk.search_lightcurvefile(target=id,
+                                       quarter=lkargs['quarter'][i],
+                                       campaign=lkargs['campaign'][i],
+                                       sector=lkargs['sector'][i],
+                                       month=lkargs['month'][i])
+        lc = tgt.download().PDCSAP_FLUX
         lc = lc.remove_nans().normalize().flatten().remove_outliers()
-
         p = lc.to_periodogram(freq_unit=units.microHertz,
-                                      normalization='psd',).flatten()
-        
-        PS_list.append((np.array(p.frequency),np.array(p.power)))
-        
+                              normalization='psd').flatten()
+        PS_list.append((np.array(p.frequency), np.array(p.power)))
     return PS_list
 
 
 class star():
     """ Class for each star to be peakbagged
-    
+
     Additional attributes are added for each step of the peakbagging process
-    
+
     Parameters
     ----------
     ID : str
         Identifier of the target. If no timeseries or powerspectrum files are
-        provided the lightcurve will be downloaded by Lightkurve. ID must 
+        provided the lightcurve will be downloaded by Lightkurve. ID must
         therefore be resolvable by Lightkurve.
     f : float, array
         Array of frequency bins of the spectrum (muHz)
     s : array
         The power at frequencies f
     numax : float
-        Initial guess for numax. Frequency of maximum power of the p-mode 
+        Initial guess for numax. Frequency of maximum power of the p-mode
         envelope (muHz)
     dnu : float
         Initial guess for dnu. Large separation of l=0 modes (muHz)
     teff : float
-        Temperature estimate for the star. Used to compute epsilon.  
+        Temperature estimate for the star. Used to compute epsilon.
     """
-    
+
     def __init__(self, ID, f, s, numax, dnu, teff):
         self.ID = ID
         self.f = f
@@ -138,25 +133,25 @@ class star():
         self.epsilon = None
         self.mode_ID = {}
         self.asy_model = None
-    
-    def asymptotic_modeid(self, d02=None, alpha=None, seff=None, 
-                          mode_width=None, env_width=None, env_height=None, 
-                          norders = 5):
+
+    def asymptotic_modeid(self, d02=None, alpha=None, seff=None,
+                          mode_width=None, env_width=None, env_height=None,
+                          norders=5):
         """ Called to perform mode ID using the asymptotic method
-        
+
         Parameters
         ----------
     d02 : float, optional
-        Initial guess for the small frequency separation (in muHz) between 
+        Initial guess for the small frequency separation (in muHz) between
         l=0 and l=2.
     alpha : float, optional
         Initial guess for the scale of the second order frequency term in the
         asymptotic relation
-    seff : float, optional 
+    seff : float, optional
         Normalized Teff
-    mode_width : float, optional 
+    mode_width : float, optional
         Initial guess for the mode width (in log10!) for all the modes that are
-        fit. 
+        fit.
     env_width : float, optional
         Initial guess for the p-mode envelope width (muHz)
     env_height : float, optional
@@ -164,83 +159,85 @@ class star():
     norders : int, optional
         Number of radial orders to fit
         """
-        
-        fit = asymptotic_fit(self, d02, alpha, seff, mode_width, env_width, env_height)
-        
+
+        fit = asymptotic_fit(self, d02, alpha, seff, mode_width, env_width,
+                             env_height)
+
         fit.run(norders)
-        
+
         self.mode_ID = fit.mode_ID
         self.asy_model = fit.asy_model
 
+
 class session():
-    """ Main class used to initiate peakbagging. 
-    
-    Use this class to initialize the star class instance(s) based on the 
+    """ Main class used to initiate peakbagging.
+
+    Use this class to initialize the star class instance(s) based on the
     provided input. Data can be provided as ascii files, or will automatically
-    be downloaded from MAST using Lightkurve. 
-        
+    be downloaded from MAST using Lightkurve.
+
     Note
     ----
-    The physical parameters, numax, dnu, teff, must each be provided at least 
-    as a list of length 2 for each star. This should containg the parameter 
-    value and it's error. Setting the error to zero will effectively fix the 
-    corresponding parameter in the fit. 
-    
+    The physical parameters, numax, dnu, teff, must each be provided at least
+    as a list of length 2 for each star. This should containg the parameter
+    value and it's error. Setting the error to zero will effectively fix the
+    corresponding parameter in the fit.
+
     Examples
     --------
     Single target, no ascii files. In this case PBjam will attempt to download
     the time series from MAST, using Lightkurve. In this case kwargs must be
-    a dictionary with the required observing season number, e.g. 
-    kwargs = {'quarter': 5} for Kepler quarter 5. Cadence and quarter month 
+    a dictionary with the required observing season number, e.g.
+    kwargs = {'quarter': 5} for Kepler quarter 5. Cadence and quarter month
     keywords can also be passed to Lightkurve through kwargs.
-    
+
     Multiple targets, no ascii files. Similar to the single target case, except
     all arguments must be in a list. kwargs must now be a dictionary of lists.
-    
+
     Single or multiple targets, with ascii files. Similar to above each target
     must have an ID and associated physical parameters. Here the timeseries
     or power spectrum can be passed to the PBjam session as either a tuple
-    or a local path to the ascii file, or a list of either. Note that if both 
+    or a local path to the ascii file, or a list of either. Note that if both
     a timeseries and spectrum are passed, the timeseries will be ignored.
-     
-    Dictionary or dataframe of targets. In this case, use the provided 
+
+    Dictionary or dataframe of targets. In this case, use the provided
     template .csv or pickled .dict file. Similar to above, a path can be
     provided, or just a target ID. In the latter case, the observing season
     must be provided in the relevant column as either an integer, or a string
-    of zeros with 1 in the bit number equivalent to the requested observing 
-    seasons.    
-    
+    of zeros with 1 in the bit number equivalent to the requested observing
+    seasons.
     """
-    
-    def __init__(self, ID=None, numax=None, dnu=None, teff=None, 
-                       timeseries=None, psd=None, dictionary=None, 
-                       dataframe=None, kwargs = {}):
-            
+
+    def __init__(self, ID=None, numax=None, dnu=None, teff=None,
+                 timeseries=None, psd=None, dictionary=None,
+                 dataframe=None, kwargs={}):
+
         physpars = [numax, dnu, teff]
         physchk = all(physpars)
-        
+
         # Given ID will use LK to download
         if ID and not timeseries and not psd:
             assert physchk, 'Must provide numax, dnu, and teff'
-        
+
             ID, numax, dnu, teff = bouncer([ID, numax, dnu, teff])
-             
-            lkargs = {}        
+
+            lkargs = {}
             for key in ['cadence', 'month', 'quarter', 'campaign', 'sector']:
                 if key in kwargs:
                     lkargs[key] = kwargs[key]
                 else:
                     lkargs[key] = [None]*len(ID)
-                    
+
                 lkargs[key] = bouncer([lkargs[key]])[0]
-            
-            PS_list  = get_psd_from_lk(ID, lkargs)
-        
-        self.stars = [star(ID[i], PS_list[i][0],PS_list[i][1], numax[i], dnu[i], teff[i]) for i in range(len(ID))]
-        
+
+            PS_list = get_psd_from_lk(ID, lkargs)
+
+        self.stars = [star(ID[i], PS_list[i][0], PS_list[i][1], numax[i],
+                      dnu[i], teff[i]) for i in range(len(ID))]
 
 
-#        
+
+      
 #        # Given path will use genfromtxt to read ascii file, must be 2 column time and relative flux, must also provude  
 #        # must also provide numax,dnu,teff
 #        elif ID and path and not timeseries and not psd:
@@ -297,13 +294,3 @@ class session():
 #        else:
 #            'Break'
 #            sys.exit()
-        
-            
-        
-
-
-        
-        
-        
-
-    
