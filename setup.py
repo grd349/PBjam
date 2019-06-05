61b1fa958e1edda4dad72541721551a850f34d35
grd349 <grd349@gmail.com>
2019-06-05 09:56:13 +0100

    Added dependencies (statsmodels+)

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='pbjam',
       packages=['pbjam'],
       #url='TODO',
-      install_requires=['numpy', 'pandas'],
+      install_requires=['numpy', 'pandas', 'emcee', 'statsmodels',
+                        'lightkurve', 'astropy', 'scipy'],
       include_package_data=True,
       )
