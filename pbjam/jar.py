856d3c740fb6d38aac2a18a99a7b919b84efa7aa
grd349 <grd349@gmail.com>
2019-06-07 15:06:00 +0100

    Added use cached function to session (bit of a hack)

--- pbjam/jar.py
+++ pbjam/jar.py
@@ -32,6 +32,7 @@ import warnings
 import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
 import os
+import glob
 
 from . import PACKAGEDIR
 
@@ -65,7 +66,7 @@ def check_list_lengths(X):
     assert lens[1:] == lens[:-1], "Provided inputs must be same length"
 
 
-def download_lc(ID, lkargs):
+def download_lc(ID, lkargs, use_cached=True):
     """ Use Lightkurve to get snr
 
     Querries MAST using Lightkurve, based on the provided target ID(s) and
@@ -103,18 +104,28 @@ def download_lc(ID, lkargs):
     source_list = []
 
     for i, id in enumerate(ID):
-        tgt = lk.search_lightcurvefile(target=id,
-                                       quarter=lkargs['quarter'][i],
-                                       campaign=lkargs['campaign'][i],
-                                       sector=lkargs['sector'][i],
-                                       month=lkargs['month'][i],
-                                       cadence=lkargs['cadence'][i])
-        lc_col = tgt.download_all()
+        if use_cached:
+            ddir = os.path.join(os.path.expanduser('~'), '.lightkurve-cache')
+            ddir += '/mastDownload/*/' + f'*{str(int(id))}*/*_llc.fits'
+            tgt = glob.glob(ddir)
+            lc_col = [lk.open(n) for n in tgt]
+        else:
+            tgt = lk.search_lightcurvefile(target=id,
+                                           quarter=lkargs['quarter'][i],
+                                           campaign=lkargs['campaign'][i],
+                                           sector=lkargs['sector'][i],
+                                           month=lkargs['month'][i],
+                                           cadence=lkargs['cadence'][i])
+            lc_col = tgt.download_all()
         lc0 = clean_lc(lc_col[0].PDCSAP_FLUX)
         for i, lc in enumerate(lc_col[1:]):
             lc0 = lc0.append(clean_lc(lc.PDCSAP_FLUX))
         lc_list.append(lc0)
-        source_list.append(tgt.table['productFilename'][0])
+        try:
+            source_list.append(tgt.table['productFilename'][0])
+        except AttributeError:
+            source_list.append(tgt)
+
     return lc_list, source_list
 
 
@@ -407,7 +418,7 @@ class session():
 
     def __init__(self, ID=None, numax=None, dnu=None, teff=None, bp_rp=None,
                  epsilon=None, timeseries=None, psd=None, dictlike=None,
-                 nthreads=1,
+                 nthreads=1, use_cached=False,
                  kwargs={}):
 
         lkwargs = kwargs.copy()  # prevents memory leak between sessions
@@ -435,7 +446,8 @@ class session():
                     lkwargs[key] = enforce_list(lkwargs[key])[0]
                 check_list_lengths(lkwargs)
 
-                lc_list, source_list = download_lc(ID, lkwargs)
+                lc_list, source_list = download_lc(ID, lkwargs,
+                                                   use_cached=use_cached)
                 PS_list = get_psd(lc_list, arr_type='TS')
 
         # Given time series as lk object, tuple or path
@@ -501,7 +513,8 @@ class session():
                         kwargs[key] = [None]*len(ID)
                 check_list_lengths(kwargs)
 
-                lc_list, source_list = download_lc(ID, kwargs)
+                lc_list, source_list = download_lc(ID, kwargs,
+                                                   use_cached=use_cached)
                 PS_list = get_psd(lc_list, arr_type='TS')
 
         else:
