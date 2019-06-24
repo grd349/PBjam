6e733e00659a41b2cce480686ed28ccb0be9d2d7
grd349 <grd349@gmail.com>
2019-06-24 13:59:01 +0100

    Added psutil & pickle dep.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='pbjam',
       packages=['pbjam'],
       #url='TODO',
       install_requires=['numpy', 'pandas', 'emcee', 'statsmodels==0.9.0',
-                        'lightkurve', 'astropy', 'scipy==1.2.1'],
+                        'lightkurve', 'astropy', 'scipy==1.2.1',
+                        'psutil', 'pickle'],
       include_package_data=True,
       )
