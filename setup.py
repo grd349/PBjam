5460fe6effca2fd5ffa323be406599f97bb96b25
Martin Nielsen <martinbonielsen.astro@gmail.com>
2019-07-17 20:12:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='pbjam',
       packages=['pbjam'],
       #url='TODO',
       install_requires=['numpy', 'pandas', 'emcee', 'statsmodels==0.9.0',
-                        'lightkurve, 'astropy', 'scipy==1.2.1',
+                        'lightkurve', 'astropy', 'scipy==1.2.1',
                         'psutil', 'corner', 'pymc3', 'matplotlib>=1.5.3'],
       include_package_data=True,
       )
