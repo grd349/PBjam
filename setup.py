5f3d6fd0ec06a365465f42462e2c4c1e450eefdd
Oliver Hall <ojh251@student.bham.ac.uk>
2019-07-18 11:37:57 +0100

    Changed statsmodels and scipy minimum conditions to 0.10.0 and 1.3.0 respectively. Runs fine

--- setup.py
+++ setup.py
@@ -3,16 +3,8 @@ from setuptools import setup
 setup(name='pbjam',
       packages=['pbjam'],
       #url='TODO',
-      install_requires=['numpy', 'pandas', 'emcee', 'statsmodels==0.9.0',
-                        'lightkurve', 'astropy', 'scipy==1.2.1',
+      install_requires=['numpy', 'pandas', 'emcee', 'statsmodels>=0.10.0',
+                        'lightkurve', 'astropy', 'scipy>=1.3.0',
                         'psutil', 'corner', 'pymc3', 'matplotlib>=1.5.3'],
       include_package_data=True,
       )
-
-
-
-
-
-
-
-
