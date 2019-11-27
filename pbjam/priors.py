f332fe40a5d2f4cf8485ad0659ff53569a06fb7a
nielsenmb <martinbonielsen.astro@gmail.com>
2019-11-27 14:08:28 +0000

    warning tweak

--- pbjam/priors.py
+++ pbjam/priors.py
@@ -115,8 +115,8 @@ class kde(plotting):
             idx = np.abs(self.prior_data.numax.values - numax[0]) < nsigma * numax[1]
             
             if not flag_warn:
-                warnings.warn(f'There are only {len(self.prior_data[idx])} stars in the prior. ' +
-                'I will expand the prior untill I have ~100 stars.')
+                warnings.warn(f'Only {len(self.prior_data[idx])} star(s) near provided numax. The prior is likely sparse in this range.' +
+                'I will try to expand the search until I have ~100 stars.')
                 flag_warn = True
                 
             if nsigma >= KDEsize:
