7db9a31a0bb946a5b9b76e436da1c59ddb22bc5e
grd349 <grd349@gmail.com>
2020-03-19 09:17:20 +0000

    Added comments to nested

--- pbjam/mcmc.py
+++ pbjam/mcmc.py
@@ -199,7 +199,29 @@ class mcmc():
 
 class nested(cpnest.model.Model):
     """
-    TODO
+    Runs CPnest to performed nested sampling from
+
+    log P(theta | D) ~ likelihood + prior
+
+    Note both likelihood and prior are in natural log.
+
+    Attributes
+    ----------
+
+    names: list, strings
+        A list of names of the model parameters
+
+    bounds: list of tuples
+        The bounds of the model parameters as [(0, 10), (-1, 1), ...]
+
+    likelihood: func
+        Function that will return the log likelihood when
+        called as likelihood(params)
+
+    prior: func
+        Function that will return the log prior when called
+        as prior(params)
+
     """
     def __init__(self, names, bounds, likelihood, prior):
         self.names=names
@@ -208,13 +230,24 @@ class nested(cpnest.model.Model):
         self.prior = prior
 
     def log_likelihood(self, param):
+        ''' Wrapper for log likelihood '''
         return self.likelihood(param.values)
 
     def log_prior(self,p):
+        ''' Wrapper for log prior '''
         if not self.in_bounds(p): return -np.inf
         return self.prior(p.values)
 
     def __call__(self):
+        '''
+        Runs the nested sampling
+
+        Returns
+        -------
+        df: pd.DataFrame
+            A dataframe of the samples produced with the
+            nested sampling
+        '''
         self.nest = cpnest.CPNest(self, verbose=0, seed=54,
                     nthreads=4,
                     nlive=100,
