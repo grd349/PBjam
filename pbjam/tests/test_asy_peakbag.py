826de1a9a73a860872eb983d35e868f3906e5526
nielsenmb <martinbonielsen.astro@gmail.com>
2020-04-06 10:00:47 -0700

    Adding comments to test functions

--- pbjam/tests/test_asy_peakbag.py
+++ pbjam/tests/test_asy_peakbag.py
@@ -7,81 +7,112 @@ import pbjam.tests.pbjam_tests as pbt
 cs = pbt.case('silly')
 
 def test_get_asy_start():
+    """Test for getting starting location of asy fit"""
+    
+    # setup
     func = cs.st.asy_fit._get_asy_start
     
+    # simple tests
     pbt.does_it_run(func, None)
     pbt.does_it_return(func, None)
     pbt.right_type(func, None, list)
     pbt.right_shape(func, None, (10,)) 
+    
+    # check function returns expected values
     assert_allclose(func(), [10, 10, 1, 10, 10, 1, 1, 1, 10, 1])
     
 def test_get_freq_range():   
+    """Test for getting frequency range of modes"""
+    
+    # setup
     func = cs.st.asy_fit._get_freq_range
-
+    
+    # simple tests
     pbt.does_it_run(func, None)
     pbt.does_it_return(func, None)
     pbt.right_type(func, None, tuple)
     pbt.right_shape(func, None, (2,))
+    
+    # check function returns expected values
     assert_allclose(func(), [-12.5, 22.5])
     
 def test_get_modeIDs():
-
+    """Test for function that gets the mode ID from asy_fit"""
+    
+    # setup
     st = cs.st
     norders = cs.pars['norders']
-    
     func = st.asy_fit.get_modeIDs 
     
+    # simple tests
     pbt.does_it_return(func, [st.asy_fit.fit, norders])
     pbt.right_type(func, [st.asy_fit.fit, norders], pd.DataFrame)
     
+    # check that median absolute deviation is zero for all parameters
     df = st.asy_fit.get_modeIDs(st.asy_fit.fit, norders)
     assert(all(df['nu_mad'].values == np.zeros(2*norders)))
+    
+    # check that median values are the same as for test setup
     assert_allclose(df['nu_med'], np.array([89.05, 90.05, 99., 100.]))
     
 def test_get_summary_stats():
 
+    # setup
     st = cs.st
-
     func = st.asy_fit._get_summary_stats #(st.asy_fit.fit)
     inp = [st.asy_fit.fit]
     
+    # simple tests
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, pd.DataFrame)
     pbt.right_shape(func, inp, (10,10))
 
     out = func(*inp)
 
+    # check that function returns the same as during testing given the above
+    # inputs
     for key in ['std', 'skew', 'MAD']:
         assert_array_equal(out.loc[:, key], 0)
-
+    
+    # same as above
     for key in ['mle', 'mean', '2nd', '16th', '50th', '84th', '97th']:
         assert_array_equal(out.loc[:,'mle'], np.array([ 1.,  2.,  1.,  0., -2.,  1.,  1.,  1.,  1.,  1.]))
   
 def test_prior_function():
+    
+    # setup
     st = cs.st
     func = st.asy_fit.prior
     inp = [cs.pars['asypars']]
-
+    
+    # simple tests
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, float)
-        
+    
+    # Test that the prior function returns -inf given the above values as
+    # during testing
     assert(func(*inp)==-np.inf)
     
 def test_likelihood_function():
+    
+    # setup
     st = cs.st
     func = st.asy_fit.likelihood
     inp = [cs.pars['asypars']]
     
+    # simple tests
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, float)
 
+    # Test that the likelihood function returns nan given the above values as
+    # during testing
     assert(np.isnan(func(*inp)))
     
 def test_asymptotic_fit_init():
-    st = cs.st
-
-    asymptotic_fit(st, norders=cs.pars['norders'])
     
+    # setup
+    st = cs.st
+    asymptotic_fit(st, norders=cs.pars['norders'])
     attributes = ['_asymptotic_relation', '_fill_diag', '_get_asy_start', 
                   '_get_enns', '_get_freq_range', '_get_nmax', 
                   '_get_summary_stats', '_log_obs', '_lor', 
@@ -93,32 +124,34 @@ def test_asymptotic_fit_init():
                   'prior', 'prior_file', 's', 'sel', 'start', 'start_samples', 
                   '_P_envelope']
     
+    # check that all attributes/methods are assigned
     pbt.assert_hasattributes(st.asy_fit, attributes)
 
 def test_asymp_spec_model_init():
     
+    # setup
     mod = asymp_spec_model(cs.st.f, cs.pars['norders'])
-    
     attributes = ['_P_envelope', '_asymptotic_relation', '_get_enns', 
                   '_get_nmax', '_lor', '_pair', 'f', 'model', 'norders']
 
+    # check that all attributes/methods are assigned
     pbt.assert_hasattributes(mod, attributes)
 
 
-def test_get_nmax():   
+def test_get_nmax():
+    
+    # setup
     nsamples = cs.pars['nsamples']
     norders = cs.pars['norders']
-
     mod = asymp_spec_model(cs.st.f, norders)
-    
     dnu, numax, eps = 10**cs.pars['asypars'][0], 10**cs.pars['asypars'][1], cs.pars['asypars'][2]
-
     func = mod._get_nmax
     inp = [dnu, numax, eps]
     inp_arr = [np.float64(dnu).repeat(nsamples), 
                np.float64(numax).repeat(nsamples),
                np.float64(eps).repeat(nsamples)]
     
+    # simple tests
     pbt.does_it_run(func, inp) 
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, float)
@@ -127,133 +160,148 @@ def test_get_nmax():
     pbt.right_shape(func, inp, ())
     pbt.right_shape(func, inp_arr, (nsamples,))    
      
+    # check that function returns the same as during testing given the above
+    # inputs
     assert(func(*inp) == 0.0)    
     
 def test_get_enns():
+    
+    # setup
     norders = cs.pars['norders']
     nmax = cs.pars['nmax']
     nsamples = cs.pars['nsamples']
-    
     mod = asymp_spec_model(cs.st.f, norders)
-
     func = mod._get_enns
     inp = [nmax, norders]
     inp_arr = [np.array([nmax]).repeat(nsamples), norders]
     
+    # simple tests
     pbt.does_it_run(func, inp)
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, np.ndarray)
     pbt.right_shape(func, inp, (norders,))
     pbt.right_shape(func, inp_arr, (nsamples, norders))
     
+    # check that function returns the same as during testing given the above
+    # inputs
     assert_array_equal(func(*inp), [0, 1])
         
 def test_asymptotic_relation():
+    
+    # setup
     norders = cs.pars['norders']
     nsamples = cs.pars['nsamples']
-    
     mod = asymp_spec_model(cs.st.f, norders)
-    
     dnu, numax, eps, alpha = 3050.0, 135.0, 1.45, 10**-2.5 # roughly solar
-    
-
-    
     func = mod._asymptotic_relation
-
     inp = [dnu, numax, eps, alpha, norders]
     inp_arr = [np.float64(numax).repeat(nsamples), 
                np.float64(dnu).repeat(nsamples), 
                np.float64(eps).repeat(nsamples), 
                np.float64(alpha).repeat(nsamples), norders]
     
+    # simple tests
     pbt.does_it_run(func, inp)
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, np.ndarray)
     pbt.right_shape(func, inp, (norders,))    
     pbt.right_shape(func, inp_arr, (norders, nsamples))
 
+    # check that function returns the same as during testing given the above
+    # inputs
     assert_allclose(func(*inp), [2896.028668, 3030.75434 ], atol = 0.001)  
       
 def test_P_envelope():
-    norders = cs.pars['norders']
     
+    # setup
+    norders = cs.pars['norders']  
     mod = asymp_spec_model(cs.st.f, norders)
-
     nus = cs.pars['freqs']
-
     envheight, numax, envwidth = 10**1.5, nus[0], 10**2.2 
-
     func = mod._P_envelope
     inp = [nus, envheight, numax, envwidth]
     
+    # simple tests
     pbt.does_it_run(func, inp)
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, np.ndarray)
     pbt.right_shape(func, inp, np.shape(nus))
   
+    # check that function returns the same as during testing given the above
+    # inputs
     assert_allclose(func(*inp), [31.6227766, 31.5718312], atol = 0.001)
     
 def test_lor():
-    norders = cs.pars['norders']
     
+    # setup
+    norders = cs.pars['norders']
     mod = asymp_spec_model(cs.st.f, norders)
-
     h, freq, w = 1, cs.st.f[0], 1
-
     func = mod._lor
     inp = [freq, h, w]
     
+    # simple tests
     pbt.does_it_run(func, inp)
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, np.ndarray)
     pbt.right_shape(func, inp, np.shape(cs.st.f))
+    
+    # check that function returns the same as during testing given the above
+    # inputs
     assert_array_equal(func(*inp), [1,1])
     
 def test_pair():
-    norders = cs.pars['norders']
-        
-    mod = asymp_spec_model([0.5, 1], norders)
-
-    h, freq, w, d02 = 1, cs.st.f[0], 1, 0.5
     
+    # setup
+    norders = cs.pars['norders']
+    mod = asymp_spec_model([0.5, 1], norders)
+    h, freq, w, d02 = 1, cs.st.f[0], 1, 0.5
     func = mod._pair
     inp = [h, freq, w, d02]
     
+    # simple tests
     pbt.does_it_run(func, inp)
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, np.ndarray)
     pbt.right_shape(func, inp, np.shape(cs.st.f))
     
+    # check that function returns the same as during testing given the above
+    # inputs
     assert_array_equal(func(*inp), [1.2,  1.35])
    
 def test_model():
-    norders = cs.pars['norders']
     
+    # setup
+    norders = cs.pars['norders']
     mod = asymp_spec_model([0.5, 1], norders)
-
     func = mod.model
     inp = cs.pars['asypars']
     
+    # simple tests
     pbt.does_it_run(func, inp)
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, np.ndarray)
     pbt.right_shape(func, inp, np.shape(cs.st.f))
     
+    # check that function returns the same as during testing given the above
+    # inputs
     assert_allclose(func(*inp), [7.93069307, 7.73076923], atol = 0.001)
 
 def test_asymp_spec_model_call():
-    norders = cs.pars['norders']
     
+    # setup
+    norders = cs.pars['norders']
     mod = asymp_spec_model([0.5, 1], norders)
-
     func = mod.model
     inp = cs.pars['asypars']
     
+    # simple tests
     pbt.does_it_run(func, inp)
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, np.ndarray)
     pbt.right_shape(func, inp, np.shape(cs.st.f))
-        
+    
+    # check that the function doesn't change the output 
     assert_allclose(mod(inp), mod.model(*inp))
 
         
--- pbjam/tests/test_priors.py
+++ pbjam/tests/test_priors.py
@@ -15,27 +15,36 @@ pdata = pd.read_csv(simp_kde.prior_file)
 solar_p = np.array([np.log10(135.0), np.log10(3050.0), 1.25,
             0.5, -2.5, 1.5, 2.2, 0.0, 3.77, 0.8])
 
+
 def test_kde_init():
     """ Test the kde init function """
     kde()
 
 def test_select_prior_data():
-
-    KDEsize = 87
+    """Tests for kde.select_prior_data"""
     
+    # Setup
+    KDEsize = 87
     func = simp_kde.select_prior_data
     
+    # Simplest run test. Given no numax it should return a sample that equals
+    # the whole prior_data.csv
     pbt.does_it_run(func, None)
     assert(len(simp_kde.prior_data) == len(pdata))
     
+    # With a given numax and KDEsize, should return KDEsize elements in the
+    # prior_data attribute
     func(to_log10(30, 10), KDEsize)
     assert(len(simp_kde.prior_data) == KDEsize)
     
 
 def test_prior_size_check():
-      
+    """Tests for kde.prior_size_check"""
+    
+    # Setup
     func = simp_kde._prior_size_check
-
+    
+    # Simple return test
     pbt.does_it_return(func, [pdata, to_log10(30, 10), 100])
     
     # These combinations should be OK
@@ -54,22 +63,27 @@ def test_prior_size_check():
         func(pdata, to_log10(300000, 1), 500)
 
 def test_prior():
+    """Tests for kde.prior"""
+    
+    # Setup
     simp_kde.kde = type('kde', (object,), {})()
     simp_kde.kde.samples = np.ones((2,10))
-        
-    # This is a silly KDE example that should return nonsense
     data = np.array(solar_p.repeat(11).reshape((10,-1)))
     simp_kde.kde = sm.nonparametric.KDEMultivariate(data=data, var_type='cccccccccc', bw='scott')
-    
     func = simp_kde.prior
     inp = [solar_p]
 
+    # basic run/return/type/shape tests
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, float)
     
+    # Should be nan given the dummy inputs above
     assert(np.isnan(func(*inp)))
             
 def test_likelihood():
+    """Tests for the kde.likelihood method"""
+    
+    # Setup
     simp_kde.kde = type('kde', (object,), {})()
     simp_kde.kde.samples = np.ones((2,10))
     simp_kde._obs = {'dnu': [solar_p[0],0.1], 'numax': [solar_p[1],0.1], 'teff': [solar_p[-2],0.1], 'bp_rp': [solar_p[-1],0.1]}
@@ -82,31 +96,37 @@ def test_likelihood():
     func = simp_kde.likelihood
     inp = [solar_p]
 
+    # basic run/return/type/shape tests
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, float)
 
+    # Should return real values < 0 for the dummy inputs above.
     assert(np.isreal(func(*inp)))
     assert(func(*inp) < 0)
 
 def test_kde_predict():
+    """Tests for the kde.kde_predict method"""
     
+    # Test raise statement
     with pytest.raises(ValueError):  
         kde().kde_predict(8)
 
+    # Setup
     err = 0.01
     simp_kde.samples = np.vstack([np.random.normal(m, err*abs(m), 50) for m in solar_p]).T
     enns = range(15,25)
-    
     func = simp_kde.kde_predict
     inp = [enns]
     
+    # basic run/return/type/shape tests
     pbt.does_it_run(func, inp)
     pbt.does_it_return(func, inp)
     pbt.right_type(func, inp, tuple)
     pbt.right_shape(func, inp, (2, 10)) 
     
-    out = simp_kde.kde_predict(enns)
-    
+    # Test that errors propogate more or less correctly (probably not the most
+    # robust test)
+    out = simp_kde.kde_predict(enns)   
     assert_almost_equal(out[1]/out[0], np.ones_like(out[1])*err, decimal = 1)
 
 
