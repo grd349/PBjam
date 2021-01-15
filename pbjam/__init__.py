544c68a5244a4045dfc290f3a218a0b14499ede0
alexlyttle <a.j.lyttle@outlook.com>
2021-01-15 18:53:05 +0000

    Added default stream handler and logger level for pbjam

--- pbjam/__init__.py
+++ pbjam/__init__.py
@@ -6,8 +6,17 @@ PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 
 import logging
 _logger = logging.getLogger(__name__)
-_logger.addHandler(logging.StreamHandler())
-# TODO: format stream handler if need be.
+_logger.setLevel('DEBUG')
+
+# if len(_logger.handlers) == 0:
+# Don't add a stream handler if any handler already exists, i.e. user knows what they're doing
+_handler = logging.StreamHandler()
+_FMT = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s', datefmt='%Y-%m-%d, %H:%M:%S',)
+_handler.setFormatter(_FMT)
+_handler.setLevel('INFO')
+_logger.addHandler(_handler)
+
+_logger.info('Importing PBjam')
 
 from .version import __version__
 from .priors import kde
