b77326b38a9b7fcc302384c974b8f94d90516162
nielsenmb <martinbonielsen.astro@gmail.com>
2023-04-21 11:15:16 +0100

    mv harvey func

--- pbjam/background.py
+++ pbjam/background.py
@@ -8,6 +8,31 @@ class bkgModel():
         self.Nyquist = Nyquist
 
     @partial(jax.jit, static_argnums=(0,))
+    def harvey(self, nu, a, b, c):
+        """ Harvey-profile
+
+        Parameters
+        ----------
+        f : np.array
+            Frequency axis of the PSD.
+        a : float
+            The amplitude (divided by 2 pi) of the Harvey-like profile.
+        b : float
+            The characeteristic frequency of the Harvey-like profile.
+        c : float
+            The exponent parameter of the Harvey-like profile.
+
+        Returns
+        -------
+        H : np.array
+            The Harvey-like profile given the relevant parameters.
+        """
+         
+        H = a / b * 1 / (1 + (nu / b)**c)
+
+        return H
+
+    @partial(jax.jit, static_argnums=(0,))
     def __call__(self, theta_u, nu):
             # Background
         H1 = self.harvey(nu, theta_u['H_power'], theta_u['H1_nu'], theta_u['H1_exp'],)
