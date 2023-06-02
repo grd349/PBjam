3687359f066a7b9b93082498eeb04dfb6ea8c51e
nielsenmb <martinbonielsen.astro@gmail.com>
2023-06-02 13:57:04 +0100

    changed init so plotting can see module attributes

--- pbjam/__init__.py
+++ pbjam/__init__.py
@@ -5,10 +5,9 @@ import os
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 
 from .version import __version__
-# from .priors import kde
-# from .session import session
-# from .asy_peakbag import asymp_spec_model, asymptotic_fit
-# from .peakbag import peakbag
-# from .star import star
-# from .mcmc import mcmc
-# from .mcmc import nested
+from .peakbag import peakbag
+from .core import star
+from .modeID import modeIDsampler
+
+#from .session import session
+ 
