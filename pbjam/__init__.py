2278aad0cceabdcfc2a59c5c309052d55d97eaf5
nielsenmb <martinbonielsen.astro@gmail.com>
2024-06-07 11:00:49 +0100

    remove numpyro

--- pbjam/__init__.py
+++ pbjam/__init__.py
@@ -5,7 +5,7 @@ import os
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 
 from .version import __version__
-from .peakbagging import DynestyPeakbag, NumPyroPeakbag
+from .peakbagging import DynestyPeakbag
 from .core import star
 from .modeID import modeIDsampler
 
