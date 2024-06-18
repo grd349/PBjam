0aecebe5e5b1a152c546f66d2ee99dbfc87a5c0f
nielsenmb <martinbonielsen.astro@gmail.com>
2024-06-18 10:17:27 +0100

    cleanup

--- pbjam/modeID.py
+++ pbjam/modeID.py
@@ -2,7 +2,7 @@ import warnings, os
 import jax.numpy as jnp
 import numpy as np
 from pbjam.l1models import Asyl1model, Mixl1model, RGBl1model
-from pbjam.pairmodel import Asyl20model
+from pbjam.l20models import Asyl20model
 from pbjam.plotting import plotting
 import pandas as pd
 
@@ -23,6 +23,8 @@ class modeIDsampler(plotting, ):
         if self.freqLimits is None:
             self.freqLimits = [self.obs['numax'][0] - self.obs['dnu'][0]*(self.N_p//2+1), 
                                self.obs['numax'][0] + self.obs['dnu'][0]*(self.N_p//2+1),]
+            
+        self.sel = (np.array(self.freqLimits).min() < self.f) & (self.f < np.array(self.freqLimits).max())    
 
         if np.isscalar(self.Npca):
             self.Npca_asy = self.Npca
@@ -42,17 +44,12 @@ class modeIDsampler(plotting, ):
         else:
             raise ValueError('PCAdims is wrong')
 
-    def runl20model(self, progress, sampler_kwargs, logl_kwargs, freqLimits=None):
+    def runl20model(self, progress, sampler_kwargs, logl_kwargs):
+ 
+        f = self.f[self.sel]
 
-        if freqLimits is None:
-            freqLimits = self.freqLimits
-        else:
-            self.freqLimits = freqLimits
-        
-        self.l20sel = (np.array(freqLimits).min() < self.f) & (self.f < np.array(freqLimits).max())    
+        s = self.s[self.sel]
 
-        f = self.f[self.l20sel]
-        s = self.s[self.l20sel]
         self.l20model = Asyl20model(f, s, 
                                     self.obs, 
                                     self.addPriors, 
@@ -68,6 +65,8 @@ class modeIDsampler(plotting, ):
 
         self.l20result = self.l20model.parseSamples(l20samples_u)
 
+        self.result = self.mergeResults(l20result=self.l20result)
+
         self.summary = {}
 
         self.summary['n_p'] = self.l20result['enn'][self.l20result['ell']==0]
@@ -76,28 +75,16 @@ class modeIDsampler(plotting, ):
 
         for key in ['numax', 'dnu', 'env_height', 'env_width', 'mode_width', 'teff', 'bp_rp']:
             self.summary[key] = self.l20result['summary'][key]
-
-        l20model = self.l20model.getMedianModel(l20samples_u)
-
-        self.l20residual = self.s[self.l20sel]/l20model
-
+ 
         return self.l20result
 
-    def runl1model(self, progress, sampler_kwargs, logl_kwargs, model='asy', freqLimits=None, BayesFactorLimit=1/2):
+    def runl1model(self, progress, sampler_kwargs, logl_kwargs, model='asy', BayesFactorLimit=1/2):
+ 
+        self.l20residual = self.s[self.sel] / self.l20model.getMedianModel()
+      
+        f = self.f[self.sel]
 
-        if freqLimits is None:
-            freqLimits = self.freqLimits
-        else:
-            if freqLimits.min() < self.freqLimits.min():
-                warnings.warn('Provided lower frequency limit is less than that of the l20 model. Results may be wonky.')
-            elif freqLimits.max() > self.freqLimits.max():
-                warnings.warn('Provided upper frequency limit is greater than that of the l20 model. Results may be wonky.')
-            
-        self.l1sel = (np.array(freqLimits).min() < self.f[self.l20sel]) & (self.f[self.l20sel] < np.array(freqLimits).max())
-        
-        f = self.f[self.l20sel][self.l1sel]
-
-        s = self.l20residual[self.l1sel]
+        s = self.l20residual
 
         if model.lower() == 'asy':
             self.l1model = Asyl1model(f, s, 
@@ -121,17 +108,19 @@ class modeIDsampler(plotting, ):
                                         NPriorSamples=50,
                                         rootiter=15,
                                         priorpath=self.priorpath,
-                                        modelChoice='complicated')
+                                        modelChoice='simple')
         else:
             raise ValueError(f'Model {model} is invalid. Please use either Asy, Mix or RGB.')
 
-        self.l1samples, self.l1logz  = self.l1model.runDynesty(progress=progress, 
+        self.l1Samples, self.l1logz  = self.l1model.runDynesty(progress=progress, 
                                                                 logl_kwargs=logl_kwargs, 
                                                                 sampler_kwargs=sampler_kwargs)
         
-        l1samples_u = self.l1model.unpackSamples(self.l1samples)
+        l1SamplesU = self.l1model.unpackSamples(self.l1Samples)
 
-        self.l1result = self.l1model.parseSamples(l1samples_u)
+        self.l1result = self.l1model.parseSamples(l1SamplesU)
+
+        self.result = self.mergeResults(l20result=self.l20result, l1result=self.l1result)
 
         return self.l1result
 
@@ -162,30 +151,56 @@ class modeIDsampler(plotting, ):
         self.runl20model(progress, sampler_kwargs, logl_kwargs)
         
         self.runl1model(progress, sampler_kwargs, logl_kwargs)
-
-        self.result = self.mergeResults(self.l20result, self.l1result)
-
-
-    def mergeResults(self, l20result, l1result):
+ 
+    def mergeResults(self, l20result=None, l1result=None, N=5000):
     
-        R = {'summary': {},
-             'samples': {}}
+        R = {'ell': np.array([]),
+             'enn': np.array([]),
+             'zeta': np.array([]),
+             'summary': {'freq'  : np.array([]).reshape((2, 0)), 
+                         'height': np.array([]).reshape((2, 0)), 
+                         'width' : np.array([]).reshape((2, 0))
+                        },
+             'samples': {'freq'  : np.array([]).reshape((N, 0)),
+                         'height': np.array([]).reshape((N, 0)), 
+                         'width' : np.array([]).reshape((N, 0))
+                        },
+            }
+ 
+        if l20result is None and hasattr(self, 'l20result'):
+            l20result = self.l20result
 
-        N = min([l20result['samples']['freq'].shape[0], l1result['samples']['freq'].shape[0]])
+            N = min([l20result['samples']['freq'].shape[0], 
+                     N])
+ 
+        if l1result is None and hasattr(self, 'l1model'):
+            l1result = self.l1result
+ 
+            N = min([l20result['samples']['freq'].shape[0], 
+                     l1result['samples']['freq'].shape[0],
+                     N])
 
-        for key in ['ell', 'enn', 'zeta']:
-            R[key] = np.append(l20result[key], l1result[key])
+        resList = [l1result, l20result] # This order overrides the numax, dnu and teff from l1result in the output since the l20result is more reliable. 
+
+        for rootkey in ['ell', 'enn', 'zeta']:
+            for D in resList:
+                if D is not None:
+                    R[rootkey] = np.append(R[rootkey], D[rootkey])
         
         for rootkey in ['summary', 'samples']:
-            for D in [l1result, l20result]: # Note this overrides the numax, dnu and teff estimates from l1result since they are included in l20result.
-                for subkey in list(D[rootkey].keys()):
-                    if subkey not in ['freq', 'height', 'width']:
-                        R[rootkey][subkey] = D[rootkey][subkey]
+            for D in resList: 
+                if D is not None:
+                    for subkey in list(D[rootkey].keys()):
+                        if subkey not in ['freq', 'height', 'width']:
+                            R[rootkey][subkey] = D[rootkey][subkey]
 
-            for subkey in ['freq', 'height', 'width']:
- 
-                R[rootkey][subkey] = np.hstack((l20result[rootkey][subkey][:N, :], l1result[rootkey][subkey][:N, :]))
-        
+                    for subkey in ['freq', 'height', 'width']:
+                        R[rootkey][subkey] = np.hstack((R[rootkey][subkey][:N, :], 
+                                                        D[rootkey][subkey][:N, :]))
+
+                else:
+                    continue
+
         return R
 
     def storeResult(self, resultDict, ID=None):
@@ -220,53 +235,4 @@ class modeIDsampler(plotting, ):
         
         df.to_csv(basefilename+'.csv', index=False)
  
-    
-# if (self.Mixl1model.DR.viableFraction <= 0.1) or self.Mixl1model.badPrior: 
-            #     print('Not enough prior samples for mixed model. Using the asymptotic model.')
-                
-            #     self.Asyl1Samples, self.Asyl1logz  = self.Asyl1model.runDynesty(progress=progress, 
-            #                                                                    logl_kwargs=logl_kwargs, 
-            #                                                                    sampler_kwargs=sampler_kwargs)
-            #     self.useMixResult = False
-                
-            # elif ((0.1 < self.Mixl1model.DR.viableFraction) and (self.Mixl1model.DR.viableFraction <= 0.90)) and not self.Mixl1model.badPrior:
-            #     print('Testing both asymptotic and mixed mode models.')
-                
-            #     if not 'nlive' in sampler_kwargs.keys():
-            #         sampler_kwargs['nlive'] = 200*self.Mixl1model.ndims
-
-            #     self.Asyl1Samples, self.Asyl1logz  = self.Asyl1model.runDynesty(progress=progress, 
-            #                                                                    logl_kwargs=logl_kwargs, 
-            #                                                                    sampler_kwargs=sampler_kwargs)
-                
-            #     self.Mixl1Samples, self.Mixl1logz  = self.Mixl1model.runDynesty(progress=progress, 
-            #                                                                    logl_kwargs=logl_kwargs, 
-            #                                                                    sampler_kwargs=sampler_kwargs)
-                
-            #     # evidence check
-            #     self.AsyMixBayesFactor = self.Asyl1logz.max() - self.Mixl1logz.max()
-
-            #     self.useMixResult = self.AsyMixBayesFactor < BayesFactorLimit              
-                 
-            # elif (0.9 <= self.Mixl1model.DR.viableFraction) and not self.Mixl1model.badPrior:
-            #     print('Using the mixed mode model.')
-
-            #     if not 'nlive' in sampler_kwargs.keys():
-            #         sampler_kwargs['nlive'] = 100*self.Mixl1model.ndims
-                
-            #     self.Mixl1Samples, self.Mixl1logz  = self.Mixl1model.runDynesty(progress=progress, 
-            #                                                                    logl_kwargs=logl_kwargs, 
-            #                                                                    sampler_kwargs=sampler_kwargs)
-                
-            #     self.useMixResult = True
-
-            # else:
-            #     print('Mixed model nan-fraction:', self.Mixl1model.DR.nanFraction)
-            #     print('Asy model nan-fraction:', self.Asyl1model.DR.nanFraction)
-            #     raise ValueError('Somethings gone wrong when picking the asy/mix model')
-
-            # if self.useMixResult:
-            #     l1samples_u = self.Mixl1model.unpackSamples(self.Mixl1Samples)
-
-            #     self.l1result = self.Mixl1model.parseSamples(l1samples_u)
-            # else:
+  
